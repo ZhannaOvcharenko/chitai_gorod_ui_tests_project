a76a4dfdd018652b48d67b22d62c47282297143b
ZhannaOvcharenko <ZOvcharenko99@gmail.com>
2025-10-26 21:43:09 +0300

    change files

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,11 +3,8 @@ import pytest
 from dotenv import load_dotenv
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
-from selene import be
-from selene.support.shared import browser
-from selene.core import config  # noqa: F401
+from selene import browser
 from utils import attach
-from pages.main_page import MainPage
 
 load_dotenv()
 
@@ -25,47 +22,26 @@ def setup_browser():
     options.add_argument('--disable-dev-shm-usage')
     options.add_argument('--disable-gpu')
 
-    if os.getenv("HEADLESS", "false").lower() in ("1", "true", "yes"):
-        options.add_argument("--headless=new")
+    if os.getenv("HEADLESS", "false").lower() == "true":
+        options.add_argument('--headless')
 
-    options.set_capability("browserName", "chrome")
-    options.set_capability("browserVersion", os.getenv("BROWSER_VERSION", "128.0"))
-    options.set_capability("selenoid:options", {
-        "enableVNC": True,
-        "enableVideo": True
-    })
-
-    if all([selenoid_login, selenoid_pass, selenoid_url]):
-        remote_url = f"https://{selenoid_login}:{selenoid_pass}@{selenoid_url}/wd/hub"
-        driver = webdriver.Remote(command_executor=remote_url, options=options)
+    if selenoid_url:
+        browser.config.driver = webdriver.Remote(
+            command_executor=f"https://{selenoid_login}:{selenoid_pass}@{selenoid_url}/wd/hub",
+            options=options,
+        )
     else:
-        driver = webdriver.Chrome(options=options)
+        browser.config.driver = webdriver.Chrome(options=options)
 
-    browser.config.driver = driver
-    browser.config.base_url = os.getenv("BASE_URL", "https://www.chitai-gorod.ru")
-    browser.config.window_width = int(os.getenv("WIDTH", 1920))
-    browser.config.window_height = int(os.getenv("HEIGHT", 1080))
-    browser.config.timeout = 10
+    browser.config.base_url = "https://www.chitai-gorod.ru"
+    browser.config.window_width = 1920
+    browser.config.window_height = 1080
 
-    yield
+    yield browser
 
-    try:
-        attach.add_screenshot(browser)
-        attach.add_logs(browser)
-        attach.add_html(browser)
-        attach.add_video(browser)
-    finally:
-        browser.quit()
+    attach.add_screenshot(browser)
+    attach.add_logs(browser)
+    attach.add_html(browser)
+    attach.add_video(browser)
 
-
-@pytest.fixture()
-def open_main_page():
-    """Открывает главную страницу и принимает cookies, если нужно."""
-    page = MainPage()
-    page.open_main_page()
-
-    cookie_buttons = browser.all("[data-testid='accept-cookies']").by(be.visible)
-    if len(cookie_buttons) > 0:
-        cookie_buttons[0].click()
-
-    return page
+    browser.quit()
